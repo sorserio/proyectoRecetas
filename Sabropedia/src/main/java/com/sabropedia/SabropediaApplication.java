38da416fcc1b2d34a2931f45844a42141d8e98b0
Daniela Bustos <daniscar.bustos@gmail.com>
2024-12-13 21:57:18 -0300

    Agregué lo realizado por Mati

---
+++ Sabropedia/src/main/java/com/sabropedia/SabropediaApplication.java
@@ -0,0 +1,13 @@
+package com.sabropedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabropediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabropediaApplication.class, args);
+	}
+
+}
